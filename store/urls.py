b56fa7fe48a02c09af04b85ac67995151a5c69d5
anirbanchakraborty123 <anirbanchakraborty714@gmail.com>
2022-01-14 22:22:08 +0530

    Added category_slug for store  page

--- store/urls.py
+++ store/urls.py
@@ -13,16 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.urls import path,include
-from django.conf.urls.static import static
-from django.conf import settings
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('' , views.home, name='home'),
-    path('store/' ,include('store.urls'), name='store'),
-]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    path('' , views.store, name='store_home'),
+    path('<slug:category_slug>/' , views.store, name='prodycus_by_category'),
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
