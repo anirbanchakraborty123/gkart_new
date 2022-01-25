fc2dcd7b4c8ab18587781c33d38347c26d65287a
anirbanchakraborty123 <anirbanchakraborty714@gmail.com>
2022-01-25 18:09:18 +0530

    upated all files

--- gkart/views.py
+++ gkart/views.py
@@ -1,13 +1,8 @@
 from django.shortcuts import render
-<<<<<<< HEAD
 from api.store.models import Product
-import logging
-log= logging.getLogger('django')
-=======
-from store.models import Product
 # import logging
 # log= logging.getLogger('django')
->>>>>>> main
+
 
 
 def home(request):
