ac811039a99e63556906e438edfe91674470bc12
anirbanchakraborty123 <anirbanchakraborty714@gmail.com>
2022-02-25 13:08:11 +0530

    AWS

--- gkart/settings.py
+++ gkart/settings.py
@@ -125,9 +125,9 @@ if 'RDS_DB_NAME' in os.environ:
         'default': {
             'ENGINE':'django.db.backends.postgresql',
             'NAME': os.environ['RDS_DB_NAME'],                     
-            'USER': os.environ['RDS_USER'] ,
+            'USER': os.environ['RDS_USERNAME'] ,
             'PASSWORD': os.environ['RDS_PASSWORD'] ,
-            'HOST': os.environ['RDS_HOST'] ,
+            'HOST': os.environ['RDS_HOSTNAME'] ,
             'PORT': os.environ['RDS_PORT'] ,
         }
     }
